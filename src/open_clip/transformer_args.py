91596da8fc8d1a6c4335616ec6ae03fbbfb5e600
Marcel99Codes <mp.marcel@outllok.de>
2025-07-11 14:16:16 +0200

    BugFix transformer arguments

--- src/open_clip/transformer_args.py
+++ src/open_clip/transformer_args.py
@@ -1,10 +1,9 @@
-
-
 tokenization_pipeline = None
 conv1_patch_size = None
 convb_patch_size = None
 
 def add_transformer_args(pTokenization_pipeline, pConv1_patch_size, pConvb_patch_size):
+    global tokenization_pipeline, conv1_patch_size, convb_patch_size
     tokenization_pipeline = pTokenization_pipeline
     conv1_patch_size = pConv1_patch_size
     convb_patch_size = pConvb_patch_size
