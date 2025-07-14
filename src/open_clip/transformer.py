BugFix transformer normalization

src/open_clip/transformer.py
@@ -543,8 +543,8 @@ class VisionTransformer(nn.Module):
         super().__init__()
         assert pool_type in ('tok', 'avg', 'none')
 
-        print(f"[DEBUG] Conv1/a patch_size (kernel, stride)={patch_size}, patch_height={patch_height}, patch_width={patch_width}")
-        print(f"[DEBUG] Conv_b patch_size (kernel, stride)={patch_size2}, patch_height={patch2_height}, patch_width={patch2_width}")
+        print(f"[DEBUG] Conv1/a patch_size (kernel, stride)={patch_size}")
+        print(f"[DEBUG] Conv_b patch_size (kernel, stride)={patch_size2}")
 
         self.output_tokens = output_tokens
         image_height, image_width = self.image_size = to_2tuple(image_size)
@@ -556,8 +556,10 @@ class VisionTransformer(nn.Module):
         self.output_dim = output_dim
 
         color_space_tokenization = transformer_args.get_tokenization_pipeline()
-        print(f"[DEBUG] Selected tokenization pipeline={color_space_tokenization}, ")
 
+        print(f"[DEBUG] Selected tokenization pipeline={color_space_tokenization}, ")
+        print(f"[DEBUG] Conv1/a patch_size (kernel, stride)={patch_size}, patch_height={patch_height}, patch_width={patch_width}")
+        print(f"[DEBUG] Conv_b patch_size (kernel, stride)={patch_size2}, patch_height={patch2_height}, patch_width={patch2_width}")
 
         if color_space_tokenization == "single":
             self.embeds = self._embeds